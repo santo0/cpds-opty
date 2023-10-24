86a6212bb7444432388b05caf12c31450422306f
Daniel Trujillo Viedma <dtviedma@gmail.com>
2023-10-24 23:01:04 +0200

    Implement the distributed version

--- opty-source-files/opty.erl
+++ opty-source-files/opty.erl
@@ -1,11 +1,33 @@
 -module(opty).
--export([start/5, stop/1]).
+-export([start/5, stop/1, startDistributed/6, stopDistributed/2]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Reads: Number of read operations per transaction
 %% Writes: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
+
+startDistributed(Clients, Entries, Reads, Writes, Time, ServerRef) ->
+    spawn(ServerRef, fun() -> register(s, server:start(Entries)) end),
+    L = startClientsDistributed(Clients, [], Entries, Reads, Writes, ServerRef),
+    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n", 
+         [Clients, Entries, Reads, Writes, Time]),
+    timer:sleep(Time*1000),
+    stopDistributed(L, ServerRef).
+
+stopDistributed(L, ServerRef) ->
+    io:format("Stopping...~n"),
+    stopClients(L),
+    waitClients(L),
+    {s, ServerRef} ! stop,
+    io:format("Stopped~n").
+
+startClientsDistributed(0, L, _, _, _, _) -> L;
+startClientsDistributed(Clients, L, Entries, Reads, Writes, ServerRef) ->
+    Pid = client:start(Clients, Entries, Reads, Writes, {s, ServerRef}),
+    startClientsDistributed(Clients-1, [Pid|L], Entries, Reads, Writes, ServerRef).
+
+
 
 start(Clients, Entries, Reads, Writes, Time) ->
     register(s, server:start(Entries)),
